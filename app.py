5a01044e6f38dba5e782fba0d1d9824ea5cf9dd5
yuchun7 <clairechen15.7@gmail.com>
2022-05-29 01:06:45 +0800

    v1 data from aiotdb

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, jsonify
+import pandas as pd
+from six.moves import urllib
+import json
+ 
+app = Flask(__name__)
+ 
+@app.route("/data.json")
+def data():
+    timeInterval = 1000
+    data = pd.DataFrame()
+    featureList = ['market-price', 
+                   'trade-volume']
+    for feature in featureList:
+        url = "https://api.blockchain.info/charts/"+feature+"?timespan="+str(timeInterval)+"days&format=json"
+        data['time'] = pd.DataFrame(json.loads(urllib.request.urlopen(url).read().decode('utf-8'))['values'])['x']*1000
+        data[feature] = pd.DataFrame(json.loads(urllib.request.urlopen(url).read().decode('utf-8'))['values'])['y']
+    result = data.to_dict(orient='records')
+    seq = [[item['time'], item['market-price'], item['trade-volume']] for item in result]
+    return jsonify(seq)
+ 
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+ 
+
+@app.route("/setRandom")
+def getData():
+    myserver ="localhost"
+    myuser="test123"
+    mypassword="test123"
+    mydb="aiotdb"
+    
+    debug =0
+    from  pandas import DataFrame as df
+    import pandas as pd                     # 引用套件並縮寫為 pd
+    import numpy as np
+
+    import pymysql.cursors
+    #db = mysql.connector.connect(host="140.120.15.45",user="toto321", passwd="12345678", db="lightdb")
+    #conn = mysql.connector.connect(host=myserver,user=myuser, passwd=mypassword, db=mydb)
+    conn = pymysql.connect(host=myserver,user=myuser, passwd=mypassword, db=mydb)
+
+    c = conn.cursor()
+ 
+
+    #====== 執行 MySQL 查詢指令 ======#
+    c.execute("SELECT * FROM sensors")
+
+    #====== 取回所有查詢結果 ======#
+    results = c.fetchall()
+    print(type(results))
+    print(results[:10])
+    if debug:
+        input("pause ....select ok..........")
+
+    test_df = df(list(results),columns=['id','time','value','temp','humi','status'])
+
+    print(test_df.head(10))
+    result = test_df.to_dict(orient='records')
+    seq = [[item['id'], item['time'], item['value'], item['temp'], item['humi'], item['status']] for item in result]
+    return jsonify(seq)
+    ######### cursor close, conn close
+    c.close()
+    conn.close()
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
